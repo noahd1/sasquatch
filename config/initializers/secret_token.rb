b37c8080a6d2ce8d11339a2be294812c1a9507b9
Noah Davis <noahd1@yahoo.com>
2014-08-25 16:50:23 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sasquatch::Application.config.secret_token = '686f9a22a1e70297586322ae14cd3e22c067ed3cdab181370079cab076d2dd0ba97c1e88504f7500d2c29e14a1ec22fcd0a18b2b0857caf41fb2b0334f2b9a72'
